917aaa525ae68d06beb84868ce3e5d73de2186ad
AnimCreate12 <78071406+AnimCreate12@users.noreply.github.com>
2021-02-08 07:55:49 +0700

    Add files via upload

---
+++ RUN.sh
@@ -0,0 +1,10 @@
+pkg install git
+pkg install nodejs
+pkg install ffmpeg
+pkg install imagemagick
+git clone https://github.com/Arya274/Arya-BOT3
+cd Arya-BOT3
+ls
+bash install.sh
+echo "Silahkan/Ketik/node index.js"
+
